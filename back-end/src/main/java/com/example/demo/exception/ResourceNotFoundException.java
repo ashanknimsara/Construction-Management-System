1d2f984dc82465aa20c53e838711c42ce942eea4
Niruni Imesha <niruniimesha@gmail.com>
2022-06-09 13:37:59 +0530

    Employee Backend

--- back-end/src/main/java/com/example/demo/exception/ResourceNotFoundException.java
+++ back-end/src/main/java/com/example/demo/exception/ResourceNotFoundException.java
@@ -1,14 +1,13 @@
-package com.example.demo.exception;
+package com.backend.springboot.exception;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@ResponseStatus(value = HttpStatus.NOT_FOUND )
 public class ResourceNotFoundException extends RuntimeException{
-
-    private static final long serialVersionUID = 1L;
-
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException (String message) {
+		super(message);
+	}
 }
