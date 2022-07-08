8d5f314e1d2666e017a1df4dc04ae25b51e15a31
Chamath Jayasekara <chamathjayasekara5962@gmail.com>
2022-07-09 00:50:13 +0530

    Update EmployeeRepository.java

--- back-end/src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ back-end/src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -1,11 +1,11 @@
-package com.constructionplanning.repository;
+package com.constructionplanning.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.constructionplanning.model.Employee;
+import com.constructionplanning.app.model.Employee;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 }
