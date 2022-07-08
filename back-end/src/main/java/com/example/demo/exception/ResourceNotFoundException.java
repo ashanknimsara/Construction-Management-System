87b7ac9938681da0b8e4a7393a27f870a954ae48
Chamath Jayasekara <chamathjayasekara5962@gmail.com>
2022-07-09 00:49:38 +0530

    Update ResourceNotFoundException.java

--- back-end/src/main/java/com/example/demo/exception/ResourceNotFoundException.java
+++ back-end/src/main/java/com/example/demo/exception/ResourceNotFoundException.java
@@ -1,13 +1,15 @@
-package com.backend.springboot.exception;
+package com.constructionplanning.app.exception;
 
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND )
-public class ResourceNotFoundException extends RuntimeException{
-	private static final long serialVersionUID = 1L;
-	
-	public ResourceNotFoundException (String message) {
-		super(message);
-	}
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+
+    }
+
 }
