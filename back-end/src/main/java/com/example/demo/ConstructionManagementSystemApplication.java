d94df50a23b7de402b3809577004e2c950d6486e
Chamath Jayasekara <chamathjayasekara5962@gmail.com>
2022-07-09 09:26:53 +0530

    Update ConstructionManagementSystemApplication.java
    Still under testing phase ,therefore each time you run the back end server user will be created with both who is with both admin and user role after testing you need to delete all the entries in the database and again run this code.

--- back-end/src/main/java/com/example/demo/ConstructionManagementSystemApplication.java
+++ back-end/src/main/java/com/example/demo/ConstructionManagementSystemApplication.java
@@ -9,5 +9,30 @@ public class ConstructionManagementSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConstructionManagementSystemApplication.class, args);
 	}
+	//to create user
+	@PostConstruct
+	protected void init() {
+	       List<Authority> authorityList = new ArrayList<>();
+	       authorityList.add(createAuthority("USER", "User role"));
+	       authorityList.add(createAuthority("ADMIN", "Admin role"));
+
+	       User user = new User();
+	       user.setPassword(passwordEncoder.encode("chamath123"));
+	       user.setUserName("Chamath");
+	       user.setFirstName("ch");
+	       user.setLastName("j");
+	       user.setEnabled(true);
+	       user.setAuthorities(authorityList);
+	       userDetailsRepository.save(user);
+
+        }
+
+	private Authority createAuthority(String roleCode, String roleDescription) {
+	       Authority authority = new Authority();
+	       authority.setRoleCode(roleCode);
+	       authority.setRoleDescription(roleDescription);
+	       return authority;
+
+	}
 
 }
