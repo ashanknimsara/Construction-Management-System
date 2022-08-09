e87020af629332def3500d12c2ee68da406df442
Pasindu <sasindunisal@gmail.com>
2022-08-09 11:30:16 +0530

    Update Item.java

--- back-end/src/main/java/com/example/demo/model/Item.java
+++ back-end/src/main/java/com/example/demo/model/Item.java
@@ -1,51 +1,76 @@
-package com.constructionplanning.app.model;
+package com.stock.demo.model;
 
-import java.util.HashSet;
-import java.util.Set;
-
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@Table(name = "item")
+@Table(name="items")
+
 public class Item {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long item_code;
-    private String item_name;
-    private double qty;
-    private String qty_type;
-    private String availability;
-    private double unit_price;
-
-    // to avoid the recusive data fetching
-    @Getter(AccessLevel.NONE)
-    @Setter(AccessLevel.NONE)
-    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "items")
-    private Set<Invoice> invoices = new HashSet<>();
-
-    public Item(String item_name, double qty, String qty_type, String availability, double unit_price) {
-        super();
-        this.item_name = item_name;
-        this.qty = qty;
-        this.qty_type = qty_type;
-        this.availability = availability;
-
-        this.unit_price = unit_price;
-    }
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long item_code;
+	
+	@Column(name="invoice_id")
+	private String invoice_id;
+	
+	@Column(name="item_name")
+	private String item_name;
+	
+	@Column(name="quantity")
+	private double qty;
+	
+	@Column(name="availability")
+	private String availability;
+	
+	public Item() { //default constructor
+		
+	}
+	
+	public Item(String invoice_id, String item_name, double qty, String availability) {
+		super();
+		this.invoice_id = invoice_id;
+		this.item_name = item_name;
+		this.qty = qty;
+		this.availability = availability;
+	}
+	
+	public long getItem_code() {
+		return item_code;
+	}
+	public void setItem_code(long item_code) {
+		this.item_code = item_code;
+	}
+	public String getInvoice_id() {
+		return invoice_id;
+	}
+	public void setInvoice_id(String invoice_id) {
+		this.invoice_id = invoice_id;
+	}
+	public String getItem_name() {
+		return item_name;
+	}
+	public void setItem_name(String item_name) {
+		this.item_name = item_name;
+	}
+	public double getQty() {
+		return qty;
+	}
+	public void setQty(double qty) {
+		this.qty = qty;
+	}
+	public String getAvailability() {
+		return availability;
+	}
+	public void setAvailability(String availability) {
+		this.availability = availability;
+	}
+	
+	
 
 }
